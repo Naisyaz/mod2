b10199fcb8ec42769d4aca0d2504a1502b968702
Nurul Aisyah <nurulaisyahazman99@gmail.com>
2025-06-10 20:49:43 +0800

    Learnt basic javascript

---
+++ session3/basic_script.js
@@ -0,0 +1,45 @@
+let x = 5
+console.log(x);
+
+x = 7
+console.log(x);
+
+const y = 8
+console.log(y);
+
+console.log((7 + 3) * 10);
+
+let seven = 7
+let three = 3
+console.log((seven + three) * 10);
+
+let banana = "Hello"
+let mango = "Mango"
+console.log(banana +mango);
+
+let one = "1"
+let two = "2"
+console.log(one + two);
+
+//Data TYPES
+//PRIMITIVE Data-Types
+//number, string, decimal (long, double)
+//Boolean can only hold 2 values, true OR false
+
+let nm = 3.1412
+console.log(typeof nm);
+let myBool = true
+console.log(typeof myBool);
+
+console.log("------------------------------------------------");
+
+//NON-PRIMITIVE Data-Types
+//object, array, function
+let anArray = [10, 9, 8, 7, 6, "Hello", true, 3.1412, true, false, true, true]
+console.log("Length of anArray: ", anArray.length)
+console.log("Last item of anArray: ", anArray[anArray.length - 1])
+
+
+
+
+
